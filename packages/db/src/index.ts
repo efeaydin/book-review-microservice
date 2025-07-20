0ed389a0e58cafb874bbd7c37fab029843e57faa
Efe Aydın <efeaydin93@gmail.com>
2025-07-20 15:25:14 +0300

    chore(db): specify dbName in mongoose connection options

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 export * from './models/index.js';
 
 export async function connectDB(url: string) {
-  await mongoose.connect(url);
+  await mongoose.connect(url, {
+    dbName: "book-review-dev",
+  });
   console.log('Connected to MongoDB');
 }
